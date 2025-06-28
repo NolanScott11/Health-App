4af9d21e7db5105415f4b2e1f1f967dbde120ab6
Nolan Scott <nmscott2@liberty.edu>
2025-06-28 16:21:54 -0400

    menu setup for health page and for fitness page

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -25,6 +25,7 @@ int main()
 		cout << "Select 1 to go to the nutrition/health page. Select 2 for the fitness page. Type 0 to quit.\nPlease make selection:";
 		
 		cin >> input;
+		//Checks input for valid selection
 		while (input != 1 && input != 2 && input != 0)
 		{
 			cout << "Invalid choice. Input must be either 1 or 2. Select again:\n";
@@ -62,8 +63,12 @@ int main()
 }
 
 //function definitions
-void healthPage()
+//HealthPage function: Opens menu and switch case for Health page.
+void healthPage() 
 {
+	int input;
+	do
+	{
 	cout << "************** Health Page **************\n\n";
 	cout << left << setw(8) << " " << "Make selection:\n";
 	cout << left << setw(8) << " " << "1: Log Food\n";
@@ -73,16 +78,74 @@ void healthPage()
 	cout << left << setw(8) << " " << "5: View goal for day\n";
 	cout << left << setw(8) << " " << "6: Create/change goals\n";
 	cout << left << setw(8) << " " << "7: Go back\n";
+	cout << "\nPlease make selection:";
+
+	cin >> input;
+	//Checks input for valid selection
+	while (input < 1 && input > 7)
+	{
+		cout << "Invalid choice. Input must be either 1 or 2. Select again:\n";
+		cin.clear();
+		cin.ignore(10000000000, '\n');
+		cin >> input;
+	}
+	switch (input)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3: 
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7: //Exits loop
+		break;
+	}
+		
+
+
+	} while (input != 7);
 
 }
 
-
+//fitnessPage function: Opens menu and switch case for fitness page.
 void fitnessPage()
 {
-	cout << "************** Fitness Page *************\n\n";
-	cout << left << setw(8) << " " << "Make selection:\n";
-	cout << left << setw(8) << " " << "1: Create Workout\n";
-	cout << left << setw(8) << " " << "2: Add lifts\n";
-	cout << left << setw(8) << " " << "3: AI model lift predicitons\n";
-	cout << left << setw(8) << " " << "4: Go back\n";
+	int input;
+	do
+	{
+		cout << "************** Fitness Page *************\n\n";
+		cout << left << setw(8) << " " << "Make selection:\n";
+		cout << left << setw(8) << " " << "1: Create Workout\n";
+		cout << left << setw(8) << " " << "2: Add lifts\n";
+		cout << left << setw(8) << " " << "3: AI model lift predicitons\n";
+		cout << left << setw(8) << " " << "4: Go back\n";
+		cout << "\nPlease make selection:";
+		cin >> input;
+		//Checks input for valid selection
+		while (input < 1 && input > 4)
+		{
+			cout << "Invalid choice. Input must be either 1 or 2. Select again:\n";
+			cin.clear();
+			cin.ignore(10000000000, '\n');
+			cin >> input;
+		}
+		switch (input)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3: 
+			break;
+		case 4: //Exits loop
+			break;
+		}
+
+	} while (input != 4);
 }
